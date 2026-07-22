f7d0cd9392ee41ad8d39a3995fc2b0a8086500ac
Dragoniconsrar <ppppqwerty5665@gmail.com>
2026-07-22 16:40:59 +0530

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,133 @@
+// Load Cart
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const cartItems = document.getElementById("cartItems");
+const subtotal = document.getElementById("subtotal");
+const delivery = document.getElementById("delivery");
+const grandTotal = document.getElementById("grandTotal");
+
+function renderCart() {
+
+    cartItems.innerHTML = "";
+
+    if(cart.length === 0){
+
+        cartItems.innerHTML = `
+            <h3 style="text-align:center;padding:40px;">
+                Your Cart is Empty 🛒
+            </h3>
+        `;
+
+        subtotal.innerText="₹0";
+        delivery.innerText="₹0";
+        grandTotal.innerText="₹0";
+
+        return;
+    }
+
+    let total=0;
+
+    cart.forEach((item,index)=>{
+
+        total += item.price * (item.qty || 1);
+
+        cartItems.innerHTML += `
+
+        <div class="cart-item">
+
+            <img src="${item.img}" class="cart-img">
+
+            <div class="cart-info">
+
+                <h3>${item.name}</h3>
+
+                <p>₹${item.price}</p>
+
+                <div class="qty">
+
+                    <button onclick="decreaseQty(${index})">−</button>
+
+                    <span>${item.qty || 1}</span>
+
+                    <button onclick="increaseQty(${index})">+</button>
+
+                </div>
+
+                <button class="remove-btn"
+                onclick="removeItem(${index})">
+
+                🗑 Remove
+
+                </button>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+    let deliveryCharge=0;
+
+    if(total<499){
+
+        deliveryCharge=49;
+
+    }
+
+    subtotal.innerText="₹"+total;
+
+    delivery.innerText=deliveryCharge==0?"FREE":"₹"+deliveryCharge;
+
+    grandTotal.innerText="₹"+(total+deliveryCharge);
+
+}
+
+function increaseQty(index){
+
+    cart[index].qty=(cart[index].qty||1)+1;
+
+    saveCart();
+
+}
+
+function decreaseQty(index){
+
+    if((cart[index].qty||1)>1){
+
+        cart[index].qty--;
+
+    }
+
+    saveCart();
+
+}
+
+function removeItem(index){
+
+    cart.splice(index,1);
+
+    saveCart();
+
+}
+
+function saveCart(){
+
+    localStorage.setItem("cart",JSON.stringify(cart));
+
+    renderCart();
+
+}
+
+document.querySelector(".order-btn").onclick=function(){
+
+    alert("🎉 Order Placed Successfully!");
+
+    localStorage.removeItem("cart");
+
+    location.href="index.html";
+
+}
+
+renderCart();
